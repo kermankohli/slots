395904d5445c67f444fa4164ae9923e110c54722
Kerman <kermankohli@gmail.com>
2025-01-29 09:46:56 +0530

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './types';
 export * from './utils/validation';
 export * from './utils/slot-helpers';
+export * from './utils/slot-set-operations';
 export * from './operations/slot-operations';
