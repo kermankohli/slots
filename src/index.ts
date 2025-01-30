64784a6d7dfd3af4118f4c33f5cb5365e219ea52
Kerman <kermankohli@gmail.com>
2025-01-30 08:36:14 +0530

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './utils/validation';
 export * from './utils/slot-helpers';
 export * from './utils/slot-set-operations';
 export * from './operations/slot-operations';
+export * from './rules';
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,3 +1,4 @@
 export * from './max-slots-per-day';
 export * from './weekday-rule';
 export * from './buffer-rule'; 
+export * from './time-of-day-rule';
